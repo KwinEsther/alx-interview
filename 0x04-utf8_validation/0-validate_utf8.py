3561aeb6a5417ecfa8f8a7d4f1a5fe046dbfd1df
Esther Nkanta <esthernkanta6@gmail.com>
2024-11-01 01:24:35 +0100

    Nade a correction to Task - 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,54 +1,52 @@
 #!/usr/bin/python3
 """
-Write a method that determines if a given data set represents
- a valid UTF-8 encoding.
-
-Prototype: def validUTF8(data)
-Return: True if data is a valid UTF-8 encoding, else return False
-A character in UTF-8 can be 1 to 4 bytes long
-The data set can contain multiple characters
-The data will be represented by a list of integers
-Each integer represents 1 byte of data, therefore you only need to
-handle the 8 least significant bits of each integer
+UTF-8 Encoding Validation
 """
 
 
 def validUTF8(data):
     """
-    utf-8 rules:
-    1 byte:  0xxxxxxx (7 bits)
-    2 bytes: 110xxxxx 10xxxxxx (11 bits)
-    3 bytes: 1110xxxx 10xxxxxx 10xxxxxx (16 bits)
-    4 bytes: 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx (21 bits)
+    Implementation
     """
-    valid = []
-    for i in data:
-        if 0 in valid:  # Check for any invalid to avoid going further the list
-            return False
-
-        if i >> 7 == 0b0:
-            valid.append(1)  # 1 means this is valid
-        elif (i >> 13 == 0b110) and (i >> 6) & 2 == 0b10:
-            valid.append(1)
-        elif i >> 20 == 0b1110 and (i >> 14) & 2 == 0b10 \
-             and (i >> 6) & 2 == 0b10:
-            valid.append(1)
-        elif i >> 27 == 0b11110 and (i >> 22) & 2 == 0b10 \
-             and (i >> 14) & 2 == 0b10 and (i >> 6) & 2 == 0b10:
-            valid.append(1)
+    point = 0
+    while point < len(data):
+        Byte1 = data[point] & 255
+        if not Byte1 >> 7:
+            point += 1
+            continue
+        elif (Byte1 >> 5) == 6:
+            try:
+                Byte2 = data[point + 1]
+                if (Byte2 >> 6) == 2:
+                    point += 2
+                    continue
+                else:
+                    return False
+            except IndexError:
+                return False
+        elif (Byte1 >> 4) == 14:
+            try:
+                Byte2 = data[point + 1]
+                Byte3 = data[point + 2]
+                if (Byte2 >> 6) == (Byte3 >> 6) == 2:
+                    point += 3
+                    continue
+                else:
+                    return False
+            except IndexError:
+                return False
+        elif (Byte1 >> 3) == 30:
+            try:
+                Byte2 = data[point + 1]
+                Byte3 = data[point + 2]
+                Byte4 = data[point + 3]
+                if (Byte2 >> 6) == (Byte3 >> 6) == (Byte4 >> 6) == 2:
+                    point += 4
+                    continue
+                else:
+                    return False
+            except IndexError:
+                return False
         else:
-            valid.append(0)  # 0 means invalid
-    return all(valid)  # all returns true if all items in a list is true or 1
-
-
-if __name__ == "__main__":
-
-    data = [65]
-    print(validUTF8(data))
-    5
-    data = [80, 121, 116, 104, 111, 110, 32, 105, 115,
-            32, 99, 111, 111, 108, 33]
-    print(validUTF8(data))
-
-    data = [229, 65, 127, 256]
-    print(validUTF8(data))
+            return False
+    return True
